ad5ae94944a9befd25606d60a86f49fe0666cde9
Shane Fonyi <shane@jayhawk.us>
2016-07-01 14:31:38 -0500

    Add files via upload

---
+++ check_cred.ps1
@@ -0,0 +1,18 @@
+$cred = Get-Credential #Read credentials
+ $username = $cred.username
+ $password = $cred.GetNetworkCredential().password
+
+ # Get current domain using logged-on user's credentials
+ $CurrentDomain = "LDAP://" + ([ADSI]"").distinguishedName
+ $domain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$UserName,$Password)
+
+if ($domain.name -eq $null)
+{
+ write-host "Authentication failed - please verify your username and password."
+  Read-Host -Prompt "Press Enter to exit"
+}
+else
+{
+ write-host "Successfully authenticated with domain $domain.name"
+  Read-Host -Prompt "Press Enter to exit"
+}
