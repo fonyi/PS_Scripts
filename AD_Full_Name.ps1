c021e93100b2cb9010319fae68d627deee47bce4
Shane Fonyi <shane@jayhawk.us>
2016-07-01 14:30:30 -0500

    Update AD_Full_Name.ps1

--- AD_Full_Name.ps1
+++ AD_Full_Name.ps1
@@ -1,9 +1,9 @@
 #Script takes a list of online IDs that separted by a return and returns a CSV with the user's online ID and their AD Display Name.
-$Users = Get-Content 'C:\Users\s223f985\Desktop\PS Scripts\username.txt'
+$Users = Get-Content
 $result=@()
 Foreach($User in $Users)
 {
 $result += get-aduser -ldapfilter "(samaccountname=$User)" -property samaccountname,displayname,mail | Select-Object -Property samaccountname,displayname,mail
 }$result|
-Export-Csv -NoTypeInformation 'C:\Users\s223f985\Desktop\PS Scripts\fullname.csv'
+Export-Csv -NoTypeInformation 'C:\Users\$USER\Desktop\fullname.csv'
 exit
