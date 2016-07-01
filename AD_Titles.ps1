6b1e7fcd62edac8b5021cd644db67f5670723ed7
Shane Fonyi <shane@jayhawk.us>
2016-07-01 14:30:07 -0500

    Update AD_Titles.ps1

--- AD_Titles.ps1
+++ AD_Titles.ps1
@@ -1,9 +1,9 @@
-#Script takes a list of online IDs that separted by a return and returns a CSV with the user's online ID and their AD Display Name.
-$Users = Get-Content 'C:\Users\s223f985\Desktop\PS Scripts\email.txt'
+#Script takes a list of email addresses that are separted by a return and returns a CSV with the user's online ID, their AD Display Name, title and department.
+$Users = Get-Content 
 $result=@()
 Foreach($User in $Users)
 {
 $result += get-aduser -ldapfilter "(mail=$User)" -property displayname,mail,title,department | Select-Object -Property displayname,mail,title, department
 }$result|
-Export-Csv -NoTypeInformation 'C:\Users\s223f985\Desktop\PS Scripts\title.csv'
+Export-Csv -NoTypeInformation 'C:\Users\$USER\Desktop\title.csv'
 exit
