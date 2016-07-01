81979b9609924b15a0c27c3cd27216174987914c
Shane Fonyi <shane@jayhawk.us>
2016-07-01 14:31:19 -0500

    Update SAM_EMAIL_Lookup.ps1

--- SAM_EMAIL_Lookup.ps1
+++ SAM_EMAIL_Lookup.ps1
@@ -1,11 +1,11 @@
 #Script takes a display name in the format "Lastname, Firstname" WITHOUT quotes separated by a return and resturns a CSV with Online ID and primary email address.
 Import-Module activedirectory
-Get-Content 'A:\Desktop\PS Scripts\users.txt' |
+Get-Content  |
 ForEach-Object{
 
 get-aduser -ldapfilter "(displayname=$_)" -property samaccountname,displayname,mail | Select-Object -Property samaccountname,mail
 
 } |
 
-Export-Csv -NoTypeInformation 'A:\Desktop\PS Scripts\results.txt'
+Export-Csv -NoTypeInformation 'C:Users\$USER\Desktop\results.txt'
 exit
