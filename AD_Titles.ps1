a96cd5831cbb899a58f5a7b12fe5579d3907624a
Shane Fonyi <shane@jayhawk.us>
2016-07-14 15:22:04 -0500

    Added file dialog box for import

--- AD_Titles.ps1
+++ AD_Titles.ps1
@@ -1,9 +1,20 @@
 #Script takes a list of email addresses that are separted by a return and returns a CSV with the user's online ID, their AD Display Name, title and department.
-$Users = Get-Content 
+$inputfile = Get-FileName "C:\temp"
+$Users = Get-Content $inputfile
 $result=@()
 Foreach($User in $Users)
 {
 $result += get-aduser -ldapfilter "(mail=$User)" -property displayname,mail,title,department | Select-Object -Property displayname,mail,title, department
 }$result|
 Export-Csv -NoTypeInformation 'C:\Users\$USER\Desktop\title.csv'
+Function Get-FileName($initialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "TXT (*.TXT)| *.TXT"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+} 
 exit
