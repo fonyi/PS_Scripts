dd04e62f0d09135e72b789e8d22ef22dacaacb85
Shane Fonyi <shane@jayhawk.us>
2016-07-14 16:15:07 -0500

    Updated to be more user friendly
    Added open file dialog. Have output go current working directory.

--- AD_Full_Name.ps1
+++ AD_Full_Name.ps1
@@ -1,9 +1,21 @@
 #Script takes a list of online IDs that separted by a return and returns a CSV with the user's online ID and their AD Display Name.
-$Users = Get-Content
+$inputfile = Get-FileName "C:\temp"
+$Users = Get-Content $inputfile
 $result=@()
+$todaysdate = get-date -date $(get-date).adddays(+0) -format yyyyMMdd
 Foreach($User in $Users)
 {
 $result += get-aduser -ldapfilter "(samaccountname=$User)" -property samaccountname,displayname,mail | Select-Object -Property samaccountname,displayname,mail
 }$result|
-Export-Csv -NoTypeInformation 'C:\Users\$USER\Desktop\fullname.csv'
+Export-Csv -NoTypeInformation "$PSScriptRoot\whois$todaysdate.csv"
+Function Get-FileName($initialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "TXT (*.txt)| *.txt"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
 exit
