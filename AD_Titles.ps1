951bed136450122cd705b52b952a8b5d3a8f33c8
Shane Fonyi <shane@jayhawk.us>
2016-07-21 09:35:24 -0500

    Moved function to the top
    Finding functions in a script is hard

--- AD_Titles.ps1
+++ AD_Titles.ps1
@@ -1,13 +1,4 @@
 #Script takes a list of email addresses that are separted by a return and returns a CSV with the user's online ID, their AD Display Name, title and department.
-$inputfile = Get-FileName "C:\temp"
-$Users = Get-Content $inputfile
-$result=@()
-$todaysdate = get-date -date $(get-date).adddays(+0) -format yyyyMMdd
-Foreach($User in $Users)
-{
-$result += get-aduser -ldapfilter "(mail=$User)" -property displayname,mail,title,department | Select-Object -Property displayname,mail,title, department
-}$result|
-Export-Csv -NoTypeInformation "$PSScriptRoot\output$todaysdate.csv"
 Function Get-FileName($initialDirectory)
 {
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
@@ -18,4 +9,13 @@ Function Get-FileName($initialDirectory)
     $OpenFileDialog.ShowDialog() | Out-Null
     $OpenFileDialog.filename
 } 
+$inputfile = Get-FileName "C:\temp"
+$Users = Get-Content $inputfile
+$result=@()
+$todaysdate = get-date -date $(get-date).adddays(+0) -format yyyyMMdd
+Foreach($User in $Users)
+{
+$result += get-aduser -ldapfilter "(mail=$User)" -property displayname,mail,title,department | Select-Object -Property displayname,mail,title, department
+}$result|
+Export-Csv -NoTypeInformation "$PSScriptRoot\output$todaysdate.csv"
 exit
