0dbfd7e3ec09042cfb5997dc9e24ab2be90b0359
Shane Fonyi <shane@jayhawk.us>
2020-07-22 19:54:01 -0400

    Add files via upload

---
+++ Create-BitLockerRecoveryPassword.ps1
@@ -0,0 +1,11 @@
+﻿$BLVol=BitLockerVolume -MountPoint $env:SystemDrive | Select *
+if($BLVol.VolumeStatus -eq "FullyEncrypted"){
+    if([string]::IsNullOrEmpty($BLVol.KeyProtector -match 'RecoveryPassword')){
+        Add-BitLockerKeyProtector -MountPoint $env:SystemDrive -RecoveryPasswordProtector
+        $BLVol=BitLockerVolume -MountPoint $env:SystemDrive | Select *
+        BackupToAAD-BitLockerKeyProtector -MountPoint $env:SystemDrive -KeyProtectorId ($BLVol.KeyProtector -match 'RecoveryPassword').KeyProtectorId
+    }
+    else{
+        BackupToAAD-BitLockerKeyProtector -MountPoint $env:SystemDrive -KeyProtectorId ($BLVol.KeyProtector -match 'RecoveryPassword').KeyProtectorId
+    }
+}
