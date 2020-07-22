0d0d2bb2620208eea0a05a7c8da294be68f08814
Shane Fonyi <shane@jayhawk.us>
2020-07-22 19:59:01 -0400

    Update Create-BitLockerRecoveryPassword.ps1

--- Create-BitLockerRecoveryPassword.ps1
+++ Create-BitLockerRecoveryPassword.ps1
@@ -2,7 +2,7 @@
 if($BLVol.VolumeStatus -eq "FullyEncrypted"){
     if([string]::IsNullOrEmpty($BLVol.KeyProtector -match 'RecoveryPassword')){
         Add-BitLockerKeyProtector -MountPoint $env:SystemDrive -RecoveryPasswordProtector
-        $BLVol=BitLockerVolume -MountPoint $env:SystemDrive | Select *
+        $BLVol = Get-BitLockerVolume -MountPoint $env:SystemDrive | Select *
         BackupToAAD-BitLockerKeyProtector -MountPoint $env:SystemDrive -KeyProtectorId ($BLVol.KeyProtector -match 'RecoveryPassword').KeyProtectorId
     }
     else{
