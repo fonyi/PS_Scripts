a7b919e78f13a7bed6241bd0b39a71dca43260b7
Shane Fonyi <shane@jayhawk.us>
2020-07-22 20:07:38 -0400

    Update Create-BitLockerRecoveryPassword.ps1

--- Create-BitLockerRecoveryPassword.ps1
+++ Create-BitLockerRecoveryPassword.ps1
@@ -1,4 +1,23 @@
-﻿$BLVol = Get-BitLockerVolume -MountPoint $env:SystemDrive | Select *
+﻿#Requires -RunAsAdministrator
+ <#	
+	.NOTES
+	===========================================================================
+	 Created by:   	Shane Fonyi (shane.fonyi@westpoint.edu)
+	 Organization: 	United States Military Academy
+	 Filename:     	Create-BitLockerRecoveryPassword.ps1
+	===========================================================================
+	.DESCRIPTION
+		This script is used to check for a Recovery Password protector for a
+        BitLocker Encrypted Drive and create a random 48 digit Recovery Password
+        if one does not exist. Then it sends it to AzureAD for escrow.
+	.INPUTS
+		NONE
+	.OUTPUTS
+		NONE
+	.EXAMPLE
+		./Create-BitLockerRecoveryPassword.ps1
+#>
+ $BLVol = Get-BitLockerVolume -MountPoint $env:SystemDrive | Select *
 if($BLVol.VolumeStatus -eq "FullyEncrypted"){
     if([string]::IsNullOrEmpty($BLVol.KeyProtector -match 'RecoveryPassword')){
         Add-BitLockerKeyProtector -MountPoint $env:SystemDrive -RecoveryPasswordProtector
